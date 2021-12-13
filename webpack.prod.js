b3a7bf47123a94b450528fcdcefb89f7f9d23b8f
ArchieRinn <brilliandar9428@gmail.com>
2021-12-13 17:40:24 +0900

    change prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,8 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [new webpack.ids.HashedModuleIdsPlugin()],
   module: {
     rules: [
       {
@@ -21,11 +18,5 @@ module.exports = merge(common, {
         ]
       }
     ]
-  },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
-    splitChunks: {
-      chunks: 'all'
-    }
   }
 })
